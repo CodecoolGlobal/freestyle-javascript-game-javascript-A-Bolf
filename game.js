9c160e3bc1aaa9f0cb58b2b747703b8da8a1b463
A-Bolf <hosc19@gmail.com>
2022-02-08 15:06:49 +0100

    add ball movement

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
+function main() {
+    initGame();
+
+}
+
 function initGame() {
     const grid = document.querySelector('.grid')
     const paddle = init_paddle(grid)
     const blocklist = init_blocks(grid)
-    init_ball(paddle, grid)
+    const ball = init_ball(paddle, grid)
+    init_event_listeners(ball, paddle)
 
 }
 function init_paddle(grid) {
@@ -14,20 +20,51 @@ function init_paddle(grid) {
     paddle.style.width = paddlewidth + 'px'
     paddle.style.left = Math.floor((gridwidth - paddlewidth) / 2) + 'px'
     grid.appendChild(paddle);
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'a' && parseInt(paddle.style.left) > 5) {
 
-            paddle.style.left = (parseInt(paddle.style.left) - 20) + 'px'
-        }
-        else if (event.key === 'd' && parseInt(paddle.style.left) < 1045) {
-            paddle.style.left = (parseInt(paddle.style.left) + 20) + 'px'
-
-        }
-    })
 
     return paddle;
 
 }
+
+function move_ball(paddle) {
+    let speed = 10
+    let bounce = true
+    ball = document.querySelector('.ball')
+    ball.style.left = parseInt(ball.style.left) + speed + "px"
+    ball.style.bottom = parseInt(ball.style.bottom) + speed + "px"
+
+    if (ball.style.bottom > 560 || ball.style.left > 1140 && bounce)
+
+
+        bounce = false
+
+
+}
+var first_shot = true;
+function init_event_listeners(ball, paddle) {
+    document.addEventListener('keydown', function (event) {
+
+        if (event.key === 'a' && parseInt(paddle.style.left) > 5) {
+
+            paddle.style.left = (parseInt(paddle.style.left) - 20) + 'px'
+            if (first_shot) ball.style.left = (parseInt(ball.style.left) - 20) + 'px'
+
+        }
+        else if (event.key === 'd' && parseInt(paddle.style.left) < 1045) {
+            paddle.style.left = (parseInt(paddle.style.left) + 20) + 'px'
+            if (first_shot) ball.style.left = (parseInt(ball.style.left) + 20) + 'px'
+
+
+
+        }
+        else if (event.key === " " && first_shot) {
+            setInterval(move_ball, 30);
+            first_shot = false;
+
+        }
+    })
+}
+
 function init_blocks(grid) {
     let left = 40
     let top = [25, 50, 75, 100]
@@ -57,8 +94,8 @@ function init_ball(paddle, grid) {
     ball.style.left = Math.floor(gridwidth / 2) - 20 + 'px'
     ball.style.bottom = '30px'
     grid.appendChild(ball);
-
+    return ball;
 
 }
 
-initGame();
+main();
