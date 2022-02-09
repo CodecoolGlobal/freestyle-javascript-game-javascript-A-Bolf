2f63e6690d3b26588355a4860000fcbe455c82b3
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-02-09 21:08:54 +0100

    init menu html

--- server.py
+++ server.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
+
+
+@app.route('/breakout')
+def game():
+    difficulty = request.args.get('difficulty')
+    return render_template('index.html', difficulty=difficulty)
